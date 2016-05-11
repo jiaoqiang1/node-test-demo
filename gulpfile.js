4e1bcb99ca607e9c6081f3995cc7d1d694a855ff
GinMu <smu@anfengde.com>
2016-05-11 16:15:23 +0800

    modify gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require("gulp");
 var jshint = require('gulp-jshint');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var mocha = require('gulp-mocha');
 var config = require('./config.json');
 gulp.task("jshint", function() {
     gulp.src(config.jshint_src)
@@ -25,6 +26,12 @@ gulp.task("minify-js", function() {
         .pipe(gulp.dest(config.uglify_dest))
 });
 
+gulp.task("mocha", function() {
+    gulp.src(config.mocha_test_src)
+        .pipe(mocha())
+});
+
 gulp.watch(config.jshint_src, ['jshint']);
 gulp.watch(config.uglify_src, ['minify-js']);
-gulp.task('default', ['jshint','minify-js']);
+gulp.watch(config.mocha_test_src, ['mocha']);
+gulp.task('default', ['jshint', 'minify-js', 'mocha']);
